d5fd30185e43796882fbd32dee177a7a54838dc4
Noorbasha <snoorbasha50@gmail.com>
2026-07-25 21:01:20 +0530

    feat:majority element 1

---
+++ Arrays/majority_element_1.ts
@@ -0,0 +1,59 @@
+// 169. Majority Element
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+// The input is generated such that a majority element will exist in the array.
+
+
+
+function majorityElement(nums: number[]): number {
+    let candidate1:number|null=null
+    let count1:number=0
+    //find possible candidates
+    for(const num of nums){
+        if(candidate1==num){
+            count1++;
+        }else if(count1==0){
+            candidate1=num
+            count1=1
+        }else{
+            count1--;
+        }
+    }
+
+    //verify candidates
+    count1=0
+    for(const num of nums){
+        if(candidate1==num){
+            count1++;
+        }
+    }
+    if(count1>nums.length/2){
+        return candidate1 as number
+    }
+    return 0
+    
+};
