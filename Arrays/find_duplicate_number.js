ffd70fb1660bb40d6ccb8dd91f658a9c00ac859d
Noorbasha <snoorbasha50@gmail.com>
2026-09-20 21:43:29 +0530

    feat:find duplicate number Floyd’s Cycle Detection Algorithm

---
+++ Arrays/find_duplicate_number.js
@@ -0,0 +1,59 @@
+// 287. Find the Duplicate Number
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and using only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+// Example 3:
+
+// Input: nums = [3,3,3,3,3]
+// Output: 3
+ 
+
+// Constraints:
+
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+
+
+
+var findDuplicate = function(nums) {
+   
+   let slow=0
+   let fast=0
+
+//find cycle exists
+   do {
+    slow=nums[slow]
+    fast=nums[nums[fast]]
+   } while(slow!==fast)
+
+//find cycle entrance
+    slow=0
+    while(slow!==fast){
+        slow=nums[slow]
+        fast=nums[fast]
+    }
+
+    return slow
+
+};
