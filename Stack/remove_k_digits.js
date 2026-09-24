404a8b0c0714851b23d1e00b70ec28cbac87fbc6
Noorbasha <snoorbasha50@gmail.com>
2026-09-24 20:07:53 +0530

    feat:remove k digits

---
+++ Stack/remove_k_digits.js
@@ -0,0 +1,54 @@
+// 402. Remove K Digits
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+ 
+
+// Example 1:
+
+// Input: num = "1432219", k = 3
+// Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+// Example 2:
+
+// Input: num = "10200", k = 1
+// Output: "200"
+// Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+// Example 3:
+
+// Input: num = "10", k = 2
+// Output: "0"
+// Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ 
+
+// Constraints:
+
+// 1 <= k <= num.length <= 105
+// num consists of only digits.
+// num does not have any leading zeros except for the zero itself.
+
+
+
+var removeKdigits = function(num, k) {
+    let monoIncStack=[]
+    for(const digit of num){
+        while(monoIncStack.length>0 && k>0 && monoIncStack[monoIncStack.length-1]>digit){
+            monoIncStack.pop()
+            k--;
+        }
+        monoIncStack.push(digit)
+    }
+    while (k > 0) {
+    monoIncStack.pop();
+    k--;
+    }
+    let result=monoIncStack.join('').replace(/^0+/,"")
+    return result===""?"0":result
+    console.log("monoIncStack",monoIncStack)
+
+
+};
